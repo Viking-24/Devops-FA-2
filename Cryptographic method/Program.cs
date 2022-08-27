5bf3538ca44c4276be96cb73523c75ee3eebd06a
Viking-24 <112281811+Viking-24@users.noreply.github.com>
2022-08-28 01:28:56 +0200

    Add files via upload

---
+++ Cryptographic method/Program.cs
@@ -0,0 +1,25 @@
+﻿class ConsoleApp
+{
+    static void Main()
+    {
+        Console.Write("Enter the message: ");
+        string str = Console.ReadLine() ?? "";
+        Console.Write("Enter the key: ");
+        string key = Console.ReadLine() ?? "";
+
+
+        int mod = key.Length;
+        int j = 0;
+        for (int i = key.Length; i < str.Length; i++)
+        {
+            key += key[j % mod];
+            j++;
+        }
+        string ans = "";
+        for (int i = 0; i < str.Length; i++)
+        {
+            ans += Convert.ToChar((key[i] - 'A' + str[i] - 'A') % 26 + 'A');
+        }
+        Console.WriteLine($"Encrypted message: {ans}");
+    }
+}
